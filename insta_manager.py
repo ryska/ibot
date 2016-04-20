f098525616233d1eaca190c3c83799921e9458e9
fifarafka <wojtasikmonikia@gmail.com>
2016-04-20 12:51:52 +0200

    added get_media_count function

--- insta_manager.py
+++ insta_manager.py
@@ -2,6 +2,19 @@ from instagram import client
 from config import CONFIG
 from bottle import request
 
+def get_media_count():
+    access_token = request.session['access_token']
+    if not access_token:
+        return 'Missing Access Token'
+    try:
+        api = client.InstagramAPI(access_token=access_token, client_secret=CONFIG['client_secret'])
+        user_count = api.user().__getattribute__('counts')
+        splitted_response = str(user_count).split(", 'followed_by': ", 1)
+        splitted_response2 = splitted_response[0].split("{'media': ", 1)
+    except Exception as e:
+        print(e)
+    return splitted_response2[1]
+
 def get_followed_by_count():
     access_token = request.session['access_token']
     if not access_token:
