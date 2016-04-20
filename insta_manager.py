0ad154b4634056fc33aa7d43e17cec97f202239e
fifarafka <wojtasikmonikia@gmail.com>
2016-04-20 13:01:47 +0200

    added get_user functions

--- insta_manager.py
+++ insta_manager.py
@@ -41,3 +41,43 @@ def get_follows_count():
     except Exception as e:
         print(e)
     return splitted_response3[0]
+
+def get_user_media_count(user_id):
+    access_token = request.session['access_token']
+    if not access_token:
+        return 'Missing Access Token'
+    try:
+        api = client.InstagramAPI(access_token=access_token, client_secret=CONFIG['client_secret'])
+        user_count = api.user(user_id).__getattribute__('counts')
+        splitted_response = str(user_count).split(", 'followed_by': ", 1)
+        splitted_response2 = splitted_response[0].split("{'media': ", 1)
+    except Exception as e:
+        print(e)
+    return splitted_response2[1]
+
+def get_user_followed_by_count(user_id):
+    access_token = request.session['access_token']
+    if not access_token:
+        return 'Missing Access Token'
+    try:
+        api = client.InstagramAPI(access_token=access_token, client_secret=CONFIG['client_secret'])
+        user_count = api.user(user_id).__getattribute__('counts')
+        splitted_response = str(user_count).split("'followed_by': ", 1)
+        splitted_response2 = splitted_response[1].split(", 'follows': ", 1)
+    except Exception as e:
+        print(e)
+    return splitted_response2[0]
+
+def get_user_follows_count(user_id):
+    access_token = request.session['access_token']
+    if not access_token:
+        return 'Missing Access Token'
+    try:
+        api = client.InstagramAPI(access_token=access_token, client_secret=CONFIG['client_secret'])
+        user_count = api.user(user_id).__getattribute__('counts')
+        splitted_response = str(user_count).split("'followed_by': ", 1)
+        splitted_response2 = splitted_response[1].split(", 'follows': ", 1)
+        splitted_response3 = splitted_response2[1].split("}",1)
+    except Exception as e:
+        print(e)
+    return splitted_response3[0]
