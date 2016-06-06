a7bec6f59addba633ed5a9a7be8654a54c419549
fifarafka <wojtasikmonikia@gmail.com>
2016-06-06 20:18:44 +0200

    DatabaseMenager refactor

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,60 +1,67 @@
 import sqlite3;
 from webscrapper import category_split, subcategory_split, title_split, hashtags_split, read_content
 
-con = sqlite3.connect('tag_database.db')
-#con.row_factory = lambda cursor, row: row[0]
-con.text_factory = str
-con.row_factory = sqlite3.Row
-cur = con.cursor()
 
-# nie odpalac
-#usuwa tabele
-def delete_tags_table():
-    cur.execute("DROP TABLE IF EXISTS Tags;")
-    con.commit()
+class DatabaseManager:
 
-# nie odpalac, chociaz mozna bo wywali blad
-#tworzy tabele tagow
-def create_tags_tables():
-    cur.execute("""
-         CREATE TABLE IF NOT EXISTS Tags (
-        tag varchar(50) NOT NULL,
-        category varchar(50) NOT NULL,
-        PRIMARY KEY (tag,category)
-        )""")
-    con.commit()
+    # inicjalizuje połączenie z tabelą
+    def __init__(self):
+        self.con = sqlite3.connect('tag_database.db')
+        self.con.text_factory = str
+        self.con.row_factory = sqlite3.Row
+        self.cur = self.con.cursor()
 
-#wstawia wiersz do tabeli
-def insert_value(tag, category):
-    cur.execute(
-        "INSERT OR IGNORE INTO Tags (tag, category) VALUES (?,?)",(tag,category,))
-    con.commit()
+    # usuwa tabele tags z bazy
+    def delete_tags_table(self):
+        self.cur.execute("DROP TABLE IF EXISTS Tags;")
+        self.con.commit()
 
-#zwraca liste tagow z zadanej kategorii
-def get_tag_by_category(category):
-    cur.execute("select tag from Tags where category=? ORDER BY random()",(category,))
-    ar = [str(item[0]) for item in cur.fetchall()]
-    return ar
+    # tworzy tabele jeśli nie istnieje
+    def create_tags_tables(self):
+        self.cur.execute("""
+          CREATE TABLE IF NOT EXISTS Tags (
+          tag varchar(50) NOT NULL,
+          category varchar(50) NOT NULL,
+          PRIMARY KEY (tag,category)
+          )""")
+        self.con.commit()
 
-#zwraca liste tagow o zadanej dlugosci i zadanej kategorii
-def get_count_tag_by_category(category, count):
-    cur.execute("select tag from Tags where category=? ORDER BY random() limit ?",(category,count))
-    ar = [str(item[0]) for item in cur.fetchall()]
-    return ar
+    # wstawia wiersz do tabeli
+    def insert_value(self, tag, category):
+        self.cur.execute(
+            "INSERT OR IGNORE INTO Tags (tag, category) VALUES (?,?)",(tag,category,))
+        self.con.commit()
 
-#zwraca liste kategorii
-def get_categories():
-    cur.execute("select distinct category from Tags ")
-    ar = [str(item[0]) for item in cur.fetchall()]
-    return ar
+    # zwraca liste tagow z zadanej kategorii
+    def get_tag_by_category(self, category):
+        self.cur.execute("select tag from Tags where category=? ORDER BY random()",(category,))
+        ar = [str(item[0]) for item in self.cur.fetchall()]
+        return ar
+
+    # zwraca liste tagow o zadanej dlugosci i zadanej kategorii
+    def get_count_tag_by_category(self, category, count):
+        self.cur.execute("select tag from Tags where category=? ORDER BY random() limit ?",(category,count))
+        ar = [str(item[0]) for item in self.cur.fetchall()]
+        return ar
+
+    # zwraca liste kategorii
+    def get_categories(self):
+        self.cur.execute("select distinct category from Tags ")
+        ar = [str(item[0]) for item in self.cur.fetchall()]
+        return ar
 
 
-#nie odpalac :D
-#wypelnia baze danych naszymi danymi
-def fill_database():
-    categories = category_split(read_content())
-    for cat in categories:
-        for sub in subcategory_split(cat):
-            category = title_split(sub)
-            for tag in hashtags_split(sub):
-                insert_value(tag,category)
+    # wypelnia baze danych naszymi danymi
+    def fill_database(self):
+        categories = category_split(read_content())
+        for cat in categories:
+            for sub in subcategory_split(cat):
+                category = title_split(sub)
+                for tag in hashtags_split(sub):
+                    self.insert_value(tag,category)
+
+
+    def scrap(self):
+        self.delete_tags_table()
+        self.create_tags_tables()
+        self.fill_database()
